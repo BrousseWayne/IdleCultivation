2b47cd4712b1ff744b070af2c89907b8cd9cf3d7
BrousseWayne <samynedir@gmail.com>
2025-08-31 19:29:56 +0200

    clean and add new page

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,17 @@ import IdleGameInterface from "./app/page";
 import IdleGameInterface2 from "./app/page2";
 import { BrowserRouter, Route, Routes } from "react-router";
 import IdleGameInterface3 from "./app/Layout/idleGameInterface";
+import IdleGameInterface4 from "./app/page3";
 // import App from './App.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
-        {/* <Route path="/1" element={<IdleGameInterface/>} /> */}
-        {/* <Route path="/2" element={<IdleGameInterface2/>} /> */}
+        <Route path="/1" element={<IdleGameInterface/>} />
+        <Route path="/2" element={<IdleGameInterface2/>} />
         <Route path="/3" element={<IdleGameInterface3/>} />
+        <Route path="/4" element={<IdleGameInterface4/>} />
     </Routes>
     </BrowserRouter>
     {/* <App /> */}

8770407266ee3061906a825e29fd085b389383d9
BrousseWayne <samynedir@gmail.com>
2025-09-02 18:30:52 +0200

    add new mock up

--- src/main.tsx
+++ src/main.tsx
@@ -7,20 +7,21 @@ import IdleGameInterface2 from "./app/page2";
 import { BrowserRouter, Route, Routes } from "react-router";
 import IdleGameInterface3 from "./app/Layout/idleGameInterface";
 import IdleGameInterface4 from "./app/page3";
+import IdleCultivationGame5 from "./app/page4";
 // import App from './App.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <Routes>
-        <Route path="/1" element={<IdleGameInterface/>} />
-        <Route path="/2" element={<IdleGameInterface2/>} />
-        <Route path="/3" element={<IdleGameInterface3/>} />
-        <Route path="/4" element={<IdleGameInterface4/>} />
-    </Routes>
+      <Routes>
+        <Route path="/1" element={<IdleGameInterface />} />
+        <Route path="/2" element={<IdleGameInterface2 />} />
+        <Route path="/3" element={<IdleGameInterface3 />} />
+        <Route path="/4" element={<IdleGameInterface4 />} />
+        <Route path="/5" element={<IdleCultivationGame5 />} />
+      </Routes>
     </BrowserRouter>
     {/* <App /> */}
     {/* <IdleGameInterface2 /> */}
   </StrictMode>
 );
-4
